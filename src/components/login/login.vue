a368caa16ed4cee00d05bf1d2129ef3ec7c3726d
zhangyu97 <396471308@qq.com>
2018-05-23 11:50:33 +0800

    dsda

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,46 +1,65 @@
 <template>
   <div id="login">
-
     <div class="top">欢迎登陆</div>
-    <form class="form-login" action="" id="form" method="post" >
-      <input type="text" class="text" name="zhanghao" id="user" placeholder="输入账号">
-      <input type="password" name="pass" id="pass" class="text" placeholder="输入密码">
-      <div class="yanzheng">
-        <input type="password" name="code" id="passs"  placeholder="验证码">
-        <img src="" alt="" class="img" id="code" onclick="">
+      <div class="form-login">
+      <!--<form class="form-login" id="form" method="post" >-->
+        <input type="text" class="text" name="zhanghao" id="user" placeholder="输入账号" v-model="input10">
+        <input type="password" name="pass" id="pass" class="text" placeholder="输入密码" v-model="input11">
+        <div class="yanzheng">
+          <input type="password" name="code" id="passs"  placeholder="验证码" v-model="input12">
+          <img src="/api/admin/login/code" alt=""
+               onclick="this.src='/api/admin/login/code?'+Math.random()">
+        </div>
+        <input type="submit" class="submit" @click="check">
+        <div class="login-social-link">
+
+        </div>
       </div>
-      <input type="submit" class="submit">
-      <div class="login-social-link">
-      </div>
-    </form>
+      <!--</form>/-->
     </div>
-
-
 </template>
 
 <script>
-    export default{
-        name: 'login',
+    export default {
         data(){
             return {
-
-
+                input10: '',
+                input11: '',
+                input12: ''
             }
 
         },
-      created(){
+        methods: {
+            check(){
+                let obj = {};
+                obj.zhanghao = this.input10;
+                obj.pass = this.input11;
+                obj.code = this.input12;
+                this.$http.post('/api/admin/login/check', obj, {
+                    headers: {
+                        "content-type": 'application/json'
+                    }
+                }).then(res => {
+                console.log(res);
+                    if (res.body!= 'ok') {
+                        alert(res.body)
+                    } else {
+                        this.$router.push('/main')
+                    }
 
-      }
+                })
+            }
+        }
     }
 </script>
 <style scoped lang='scss'>
   body{
     width: 100%;
-    height: auto;
+    height: 100%;
   }
   #login{
     width: 100%;
-    height: auto;
+    height: 100%;
     background: url("../../assets/img/bg.jpg");
 
     .top{
@@ -58,16 +77,16 @@
       width: 300px;
       height: 335px;
       padding: 0 10px;
-      margin: 0 auto;
+      margin: 0 auto 100px;
       >input{
         width: 280px;
         height: 49px;
         margin: 10px;
-        border: none;
         box-sizing: border-box;
         outline-style: none;
         border-radius: 5px;
-        background: #262729;
+        background: #f7f7f7;
+        border:1px solid #7E838E;
       }
       input.text{
         padding-left: 20px;
@@ -84,9 +103,9 @@
           box-sizing: border-box;
           margin-right: 5px;
           border-radius: 5px;
-          border: none;
+          border:1px solid #7E838E;
           padding-left: 20px;
-          background: #262729;
+          background: #f7f7f7;
         }
         img{
           width: 135px;
