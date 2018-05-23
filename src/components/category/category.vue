47ca05c50cebfb604df38b18447e2c9cb290a5f8
MalikCheng <296062888@qq.com>
2018-05-23 08:57:10 +0800

    cakeadmin

--- src/components/category/category.vue
+++ src/components/category/category.vue
@@ -2,10 +2,8 @@
     <el-table
             :data="tableData"
             style="width: 100%">
-        <!--<el-table-column-->
-                <!--type="index"-->
-                <!--index="">-->
-        <!--</el-table-column>-->
+        <el-table-column label="ID" prop="id" width="60">
+        </el-table-column>
         <el-table-column
                 label="名称"
                 width="180">
@@ -18,14 +16,16 @@
         <el-table-column label="操作">
             <template slot-scope="scope">
                 <el-button size="mini">
-                    <router-link>
+                    <router-link to="/api/admin/cateedit">
                         编辑
                     </router-link>
                 </el-button>
                 <el-button
                         size="mini"
                         type="danger"
-                        @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                        @click="handleDelete(scope.$index, scope.row)">
+                    <router-link to="/api/admin/catedel">删除</router-link>
+                </el-button>
             </template>
         </el-table-column>
     </el-table>
@@ -40,7 +40,7 @@
         },
         created(){
             this.$http.get('/api/admin/category').then(res=>{
-                console.log(res);
+                this.tableData = res.body;
             })
         },
         methods: {
